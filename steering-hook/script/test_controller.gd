060c2f043783a6f7f6eb8949ee1b17371ecd9562
vent420 <vincentmott2006@gmail.com>
2026-09-17 16:09:55 +0200

    testing collisions

--- steering-hook/script/test_controller.gd
+++ steering-hook/script/test_controller.gd
@@ -1,11 +1,20 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var speed = 400
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	speed = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
-	move_and_slide()
+	
+	
+
+
+
+
+
+func _on_body_entered(body) -> void:
+	print("collided")
+	pass # Replace with function body.
