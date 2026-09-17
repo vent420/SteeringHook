67304b1fbc21d09b3260eb74d1da28e26904d43e
kaileythealien <kaileythealien0@gmail.com>
2026-09-17 16:01:55 +0200

    bigass commit :-)

--- steering-hook/tiles/tile_road_color_change.gd
+++ steering-hook/tiles/tile_road_color_change.gd
@@ -1,10 +1,11 @@
 extends Sprite2D
 
-@export var spr_arr: Array[Texture2D]
+@export var defaultPath: String
 
 func _ready() -> void:
-	pass
+	if defaultPath.is_empty(): defaultPath = texture.resource_path.left(-4)
 	# xxx.connect(ChangeRoadColor)
+	ChangeRoadColor("r")
 
-func ChangeRoadColor(level: int):
-	texture = spr_arr[level]
+func ChangeRoadColor(col: String):
+	texture = load(defaultPath + "_" + col + ".png")
