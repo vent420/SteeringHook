0c4d318402d4a6f7c1f33839ff9598ca9047bdb9
KyioEkara <57576476+KyioEkara@users.noreply.github.com>
2026-09-18 09:03:01 +0200

    rien d'important lol

--- steering-hook/tiles/tile_road_color_change.gd
+++ steering-hook/tiles/tile_road_color_change.gd
@@ -5,9 +5,11 @@ extends Sprite2D
 func _ready() -> void:
 	if defaultPath.is_empty(): defaultPath = texture.resource_path.left(-4)
 	# xxx.connect(ChangeRoadColor)
-	ChangeRoadColor("r")
+	ChangeRoadColor("m")
 
 func ChangeRoadColor(col: String):
 	var path_to_new_file: String = defaultPath + "_" + col + ".png"
 	if ResourceLoader.exists(path_to_new_file):
-		texture = load(defaultPath + "_" + col + ".png")
+		texture = load(path_to_new_file)
+	else:
+		print(path_to_new_file)
