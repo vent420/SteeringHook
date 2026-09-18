73708382e9cf06e978ef761f59e74941e0432d8c
kaileythealien <kaileythealien0@gmail.com>
2026-09-18 15:21:28 +0200

    main menu sound

--- steering-hook/interface/main_menue.gd
+++ steering-hook/interface/main_menue.gd
@@ -4,4 +4,9 @@ func _on_quit_b_pressed() -> void:
 	get_tree().quit()
 
 func _on_texture_button_button_down() -> void:
-		get_tree().change_scene_to_file("res://interface/level_selection.tscn")
+	GlobalsAudio.play_oneshot("ui_click")
+	get_tree().change_scene_to_file("res://interface/level_selection.tscn")
+
+
+func _on_texture_button_mouse_entered() -> void:
+	GlobalsAudio.play_oneshot("ui_hover",0.5)
