770f406edb9ddc51fa111e58cb676afe27fe4b8f
kaileythealien <kaileythealien0@gmail.com>
2026-09-18 12:12:21 +0200

    death ui scene

--- steering-hook/interface/main_menue.gd
+++ steering-hook/interface/main_menue.gd
@@ -10,3 +10,9 @@ func _on_play_b_pressed() -> void:
 func _on_quit_b_pressed() -> void:	
 	get_tree().quit()
 	pass
+
+"""
+CODE TO RELOAD SCENE
+"""
+func _on_retry_button_down() -> void:
+	pass # Replace with function body.
